222519c3e473391beba40d00b45dab7fa0f833b7
mingogog <mingogog@gmail.com>
2023-08-02 16:36:25 -0400

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import './App.css'
 import Task from './Task'
 import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 import './index.css'
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([])
+  const handleDeleteTask = (taskId:number)=>{
+    setTasks(prev =>{
+      return prev.filter(t=>t.id !== taskId)
+    }) 
+  }
+  const handleTaskSubmit = (data:Task)=>{
+    setTasks(prev=>{
+      return [...prev, {...data}]
+    })
+  }
   return (
     <>
-      <TaskForm />
+      <TaskForm
+        submitTask={handleTaskSubmit}
+      />
+      <TaskList
+        tasks={tasks}
+        deleteTask={handleDeleteTask}
+      />
     </>
   )
 }
